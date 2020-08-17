793e1a238fdc77089a5a5ffaa1764b415c25d99c
Mark Brown <marksbrown@gmail.com>
2020-08-17 15:42:37 -0400

    Update Formatting
    Update formatting from the VS Code Elm extension

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -2,18 +2,21 @@ module PhotoGroove exposing (main)
 
 import Browser
 import Html exposing (..)
-import Html.Attributes as Attr exposing (class, src, id, classList, name, type_, title)
+import Html.Attributes as Attr exposing (class, classList, id, name, src, title, type_)
 import Html.Events exposing (onClick)
 import Http
-import Json.Decode exposing ( Decoder, int, string, succeed, list)
+import Json.Decode exposing (Decoder, int, list, string, succeed)
 import Json.Decode.Pipeline exposing (optional, required)
 import Json.Encode as Encode
+import List
 import Random
 import String
-import List
+
 
 urlPrefix : String
-urlPrefix = "http://elm-in-action.com/"
+urlPrefix =
+    "http://elm-in-action.com/"
+
 
 type Msg
     = ClickedPhoto String
@@ -22,16 +25,20 @@ type Msg
     | GotRandomPhoto Photo
     | GotPhotos (Result Http.Error (List Photo))
 
+
 view : Model -> Html Msg
 view model =
-    div [ class "content"] <|
+    div [ class "content" ] <|
         case model.status of
             Loaded photos selectedUrl ->
                 viewLoaded photos selectedUrl model.chosenSize
+
             Loading ->
                 []
+
             Errored errorMessage ->
                 [ text ("Error: " ++ errorMessage) ]
+
 
 viewFilter : String -> Int -> Html Msg
 viewFilter name magnitude =
@@ -45,21 +52,22 @@ viewFilter name magnitude =
         , label [] [ text (String.fromInt magnitude) ]
         ]
 
+
 viewLoaded : List Photo -> String -> ThumbnailSize -> List (Html Msg)
 viewLoaded photos selectedUrl chosenSize =
     [ h1 [] [ text "Photo Groove" ]
     , button
-        [ onClick ClickedSurpriseMe]
+        [ onClick ClickedSurpriseMe ]
         [ text "Surprise Me!" ]
     , div [ class "filters" ]
         [ viewFilter "Hue" 0
         , viewFilter "Ripple" 0
         , viewFilter "Noise" 0
         ]
-    , h3 [ ] [ text "Thumbnail Size:" ]
+    , h3 [] [ text "Thumbnail Size:" ]
     , div [ id "choose-size" ]
         (List.map viewSizeChooser [ Small, Medium, Large ])
-    , div[ id "thumbnails", class (sizeToString chosenSize) ]
+    , div [ id "thumbnails", class (sizeToString chosenSize) ]
         (List.map (viewThumbnail selectedUrl) photos)
     , img
         [ class "large"
@@ -68,42 +76,51 @@ viewLoaded photos selectedUrl chosenSize =
         []
     ]
 
+
 viewThumbnail : String -> Photo -> Html Msg
-viewThumbnail selectedUrl thumb = 
+viewThumbnail selectedUrl thumb =
     img
-        [src(urlPrefix ++ thumb.url)
+        [ src (urlPrefix ++ thumb.url)
         , title (thumb.title ++ " [" ++ String.fromInt thumb.size ++ " KB]")
         , classList [ ( "selected", selectedUrl == thumb.url ) ]
         , onClick (ClickedPhoto thumb.url)
-        ] 
+        ]
         []
 
+
 viewSizeChooser : ThumbnailSize -> Html Msg
-viewSizeChooser size = 
+viewSizeChooser size =
     label []
         [ input [ type_ "radio", name "size", onClick (ClickedSize size) ] []
         , text (sizeToString size)
         ]
 
+
 sizeToString : ThumbnailSize -> String
 sizeToString size =
     case size of
-        Small -> 
+        Small ->
             "small"
+
         Medium ->
             "med"
-        Large -> 
+
+        Large ->
             "large"
+
 
 type ThumbnailSize
     = Small
     | Medium
     | Large
-type alias Photo = 
-    { url : String 
+
+
+type alias Photo =
+    { url : String
     , size : Int
     , title : String
     }
+
 
 photoDecoder : Decoder Photo
 photoDecoder =
@@ -112,63 +129,81 @@ photoDecoder =
         |> required "size" int
         |> optional "title" string "(untitlted)"
 
+
 type Status
     = Loading
     | Loaded (List Photo) String
     | Errored String
 
-type alias Model = 
+
+type alias Model =
     { status : Status
     , chosenSize : ThumbnailSize
     }
 
+
 initialModel : Model
-initialModel = 
+initialModel =
     { status = Loading
     , chosenSize = Medium
     }
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of 
+    case msg of
         GotRandomPhoto photo ->
-            ({ model | status = selectUrl photo.url  model.status }, Cmd.none)
+            ( { model | status = selectUrl photo.url model.status }, Cmd.none )
+
         ClickedPhoto url ->
-            ({ model | status = selectUrl url model.status }, Cmd.none)
+            ( { model | status = selectUrl url model.status }, Cmd.none )
+
         ClickedSize size ->
-            ({ model | chosenSize = size }, Cmd.none)
+            ( { model | chosenSize = size }, Cmd.none )
+
         ClickedSurpriseMe ->
             case model.status of
                 Loaded (firstPhoto :: otherPhotos) _ ->
                     Random.uniform firstPhoto otherPhotos
                         |> Random.generate GotRandomPhoto
                         |> Tuple.pair model
+
                 Loaded [] _ ->
-                    ( model, Cmd.none)
+                    ( model, Cmd.none )
+
                 Loading ->
-                    ( model, Cmd.none)
+                    ( model, Cmd.none )
+
                 Errored _ ->
-                    ( model, Cmd.none)
+                    ( model, Cmd.none )
+
         GotPhotos (Ok photos) ->
             case photos of
                 first :: _ ->
-                    ({ model | status = Loaded photos first.url}
+                    ( { model | status = Loaded photos first.url }
                     , Cmd.none
                     )
+
                 [] ->
-                    ({ model | status = Errored "0 photos found"}, Cmd.none)
+                    ( { model | status = Errored "0 photos found" }, Cmd.none )
+
         GotPhotos (Err _) ->
-            (model, Cmd.none)
+            ( model, Cmd.none )
+
 
 selectUrl : String -> Status -> Status
-selectUrl url status = 
+selectUrl url status =
     case status of
         Loaded photos _ ->
             Loaded photos url
+
         Loading ->
-            status --thought
+            status
+
+        --thought
         Errored _ ->
             status
+
 
 initialCmd : Cmd Msg
 initialCmd =
@@ -177,15 +212,17 @@ initialCmd =
         , expect = Http.expectJson GotPhotos (list photoDecoder)
         }
 
+
 main : Program () Model Msg
-main = 
+main =
     Browser.element
-        { init = \_ -> (initialModel, initialCmd)
+        { init = \_ -> ( initialModel, initialCmd )
         , view = view
         , update = update
         , subscriptions = \_ -> Sub.none
         }
 
+
 rangeSlider : List (Attribute msg) -> List (Html msg) -> Html msg
-rangeSlider attributes children = 
+rangeSlider attributes children =
     node "range-slider" attributes children
