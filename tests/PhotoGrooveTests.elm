d5139b294c74b89841b732289b0ab089724691e4
Mark Brown <marksbrown@gmail.com>
2020-09-05 11:21:51 -0400

    Add view test

--- tests/PhotoGrooveTests.elm
+++ tests/PhotoGrooveTests.elm
@@ -2,11 +2,25 @@ module PhotoGrooveTests exposing (..)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
+import Html.Attributes as Attr exposing (src)
 import Http exposing (Expect)
 import Json.Decode as Decode exposing (decodeValue)
 import Json.Encode as Encode
 import PhotoGroove exposing (Model, Msg(..), Photo, initialModel, update)
 import Test exposing (..)
+import Test.Html.Query as Query
+import Test.Html.Selector exposing (attribute, tag, text)
+
+
+noPhotosNoThumbnails : Test
+noPhotosNoThumbnails =
+    test "No thumbnails render when there are no photos to render." <|
+        \_ ->
+            initialModel
+                |> PhotoGroove.view
+                |> Query.fromHtml
+                |> Query.findAll [ tag "img" ]
+                |> Query.count (Expect.equal 0)
 
 
 decoderTest : Test
