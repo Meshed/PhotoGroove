360b8d2a12ddfd41c21536af4342134f8cc389f7
Mark Brown <marksbrown@gmail.com>
2020-09-08 21:33:44 -0400

    Add PhotoFolders page to app

---
+++ src/PhotoFolders.elm
@@ -0,0 +1,67 @@
+module PhotoFolders exposing (main)
+
+import Browser
+import Html exposing (Html, h1, text)
+import Html.Attributes exposing (class, src)
+import Html.Events exposing (onClick)
+import Http
+import Json.Decode as Decode exposing (Decoder, int, list, string)
+import Json.Decode.Pipeline exposing (required)
+
+
+type alias Model =
+    { selectedPhotoUrl : Maybe String
+    }
+
+
+type Msg
+    = ClickedPhoto String
+    | GotInitialModel (Result Http.Error Model)
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+view : Model -> Html Msg
+view model =
+    h1 [] [ text "The Grooviest Folders the world has ever seen" ]
+
+
+initialModel : Model
+initialModel =
+    { selectedPhotoUrl = Nothing }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( initialModel
+    , Http.get
+        { url = "http://elm-in-action.com/folders/list"
+        , expect = Http.expectJson GotInitialModel modelDecoder
+        }
+    )
+
+
+modelDecoder : Decoder Model
+modelDecoder =
+    Decode.succeed initialModel
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ClickedPhoto url ->
+            ( { model | selectedPhotoUrl = Just url }, Cmd.none )
+
+        GotInitialModel (Ok newModel) ->
+            ( newModel, Cmd.none )
+
+        GotInitialModel (Err _) ->
+            ( model, Cmd.none )
